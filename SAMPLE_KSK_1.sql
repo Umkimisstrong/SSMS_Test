c437f4af2eaf4496728df18f0a2ad5f7cbc23c1d
KIM SANG KI <ckacl0233@gmail.com>
2022-08-01 11:23:57 +0900

    user

---
+++ SAMPLE_KSK_1.sql
@@ -0,0 +1,15 @@
+-- Logtrace
+
+SELECT USER;
+--==>>> logtraceuser
+
+SELECT *
+FROM TB_SAMPLE_USER;
+/*
+12312312	czc			zxczx		19961031	zxc		NULL		NULL	NULL	NULL	SUMMER	N	2022-07-28 14:33:53.790	ghkim2	2022-07-28 14:33:53.790	ghkim2
+2342tsd		123123123	asdasdasd	19961004	鞄姥	企鉢		畷胃asd	公廃陥	呪舛	WINTER	N	2022-07-29 08:35:46.033	ghkim2	2022-07-29 08:35:46.033	ghkim2
+けじしい	けいし		けいし		19961031	けいし	NULL		NULL	NULL	NULL	0		N	2022-07-28 17:25:00.837	ghkim2	2022-07-28 17:25:00.837	ghkim2
+しけいし	けいしけい	しけいし	19961031	鞄姥	けいしけい	しけい	しけ	いし	SUMMER	N	2022-07-28 17:21:12.907	ghkim2	2022-07-28 17:21:12.907	ghkim2
+dfg			dfg			dfgdfgdfg	19961031	xcv		xcv			xcv		xcv		xcv		AUTUMN	N	2022-07-28 14:34:40.293	ghkim2	2022-07-28 14:34:40.293	ghkim2
+*/
+
